96ecccd8ec1461a5ba73c3aca79788748b7b9f80
Jared Forsyth <jared@jaredforsyth.com>
2018-01-06 19:46:29 -0700

    not quiet

--- get-ndk.sh
+++ get-ndk.sh
@@ -1,11 +1,11 @@
 
 if [[ `uname` == "Darwin" ]]; then
-  wget --output-document=android-ndk.zip --quiet https://dl.google.com/android/repository/android-ndk-r11c-darwin-x86_64.zip
+  wget --output-document=android-ndk.zip https://dl.google.com/android/repository/android-ndk-r11c-darwin-x86_64.zip
   unzip android-ndk.zip
   rm android-ndk.zip
   mv android-ndk-r11c android-ndk
 else
-  wget --output-document=android-ndk.zip --quiet https://dl.google.com/android/repository/android-ndk-r12b-linux-x86_64.zip
+  wget --output-document=android-ndk.zip https://dl.google.com/android/repository/android-ndk-r12b-linux-x86_64.zip
   unzip android-ndk.zip
   rm android-ndk.zip
   mv android-ndk-r12b android-ndk
